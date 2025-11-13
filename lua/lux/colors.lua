57b8ca7f3ea4a430c3092a34809f292d22ded415
Jonah Brupbacher <jonahtylerb@gmail.com>
2025-11-13 11:56:17 -0500

    fix: gutter color

--- lua/lux/colors.lua
+++ lua/lux/colors.lua
@@ -380,7 +380,7 @@ function colors.generate(variation)
   colors.panel_bg = colors.black
   colors.panel_shadow = colors.black
   colors.panel_border = colors.black
-  colors.gutter_normal = ui.gray[600]
+  colors.gutter_normal = ui.gray[900]
   colors.gutter_active = ui.gray[400]
   colors.selection_bg = ui.gray[700]
   colors.selection_inactive = ui.gray[900]
